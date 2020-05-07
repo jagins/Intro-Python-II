42f7f8159f4dc59e247a66072bd2cda515f8eafb
Joshua Agins <56888002+jagins@users.noreply.github.com>
2020-05-07 14:36:37 -0500

    added addItem and dropItem in player class

--- src/player.py
+++ src/player.py
@@ -1,10 +1,14 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
 #
+from item import Item
+
 class Player:
-    def __init__(self, name, currentRoom):
+    def __init__(self, name, currentRoom, itemList = []):
         self.__name = name
         self.__currentRoom = currentRoom
+        self.__itemList = itemList
+        self.__itemCount = 2
 
     def getName(self):
         return self.__name
@@ -14,6 +18,16 @@ class Player:
 
     def setRoom(self, room):
         self.__currentRoom = room
+
+    def addItem(self, item):
+        if(self.__itemList < 2):
+            self.__itemList.append(Item(item))
+        else:
+            print('Inventory is full can only carry 2 items at a time')
+    
+    def dropItem(self, item):
+        self.__itemList.remove(item)
+
     
     def __str__(self):
         return f'{self.__name} is in {self.__currentRoom}'
